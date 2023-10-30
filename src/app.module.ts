05b1c07c07166bf97471177276266d59e811f02d
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-10-30 12:24:10 +0100

    Implement config dependencies and implement pg to applications

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './modules/App/app.controller';
+import { AppService } from './modules/App/app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validate } from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeormCliConfig from '../typeorm-cli.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      validate,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => typeormCliConfig.options,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ typeorm-cli.config.ts
@@ -0,0 +1,25 @@
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+
+config();
+
+const configService = new ConfigService();
+
+function baseFolder(): string {
+  const regex = /common+(\/|\\)+config/gi;
+  return __dirname.replace(regex, '');
+}
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.get<string>('POSTGRES_HOST'),
+  port: configService.get<number>('POSTGRES_PORT'),
+  username: configService.get<string>('POSTGRES_USER'),
+  password: configService.get<string>('POSTGRES_PASSWORD'),
+  database: configService.get<string>('POSTGRES_DB'),
+  logging: configService.get<boolean>('POSTGRES_LOGGING'),
+  synchronize: configService.get<boolean>('POSTGRES_SYNCHRONIZE'),
+  entities: [baseFolder() + '/src/models/**/*{.ts,.js}'],
+  migrations: [baseFolder() + '/src/database/migrations/**/*{.ts,.js}'],
+});
