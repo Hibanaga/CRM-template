be73e0d16454dc6752046eafba0f9e11c111584a
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-11-05 11:30:00 +0100

    Imports jwtStrategy to JWT

--- src/modules/Auth/authentication/strategy/jwt-strategy.ts
+++ src/modules/Auth/authentication/strategy/jwt-strategy.ts
@@ -1,15 +1,15 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey:
-        '487109cf168d507226080741a26b209be5cdc120310f13a9637c09a39f5c81fe',
+      secretOrKey: configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
     });
   }
 
