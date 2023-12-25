d5aac8f26138fc5a88c9ee20cdce329bc789c721
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-12-25 11:13:03 +0100

    Modify unit tests to auth controller

--- src/tests/auth/auth.controller.spec.ts
+++ src/tests/auth/auth.controller.spec.ts
@@ -10,9 +10,20 @@ import { JwtStrategy } from '../../modules/Auth/authentication/strategy/jwt-stra
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from '../../app.module';
 import { JWTConfiguration } from '../../config/authorization';
+import { INestApplication } from '@nestjs/common';
+import { Repository } from 'typeorm';
 
 describe('AuthController', () => {
+  let app: INestApplication;
   let controller: AuthController;
+  let service: AuthService;
+  let repository: Repository<User>;
+
+  const newUser = {
+    email: 'test@gmail.com',
+    password: '1234',
+    username: null,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -34,9 +45,44 @@ describe('AuthController', () => {
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
+
+    app = module.createNestApplication();
+    repository = module.get('UserRepository');
+
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await repository.query(`DELETE FROM users;`);
+  });
+
+  afterAll(() => {
+    app.close();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be called register method', async () => {
+    const user = await controller.register(newUser);
+    expect(user['email']).toBe(newUser.email);
+    expect(!!user['accessToken']).toBeTruthy();
+  });
+
+  it('should be called login method', async () => {
+    const user = await controller.register(newUser);
+    const res = await controller.login({
+      email: user.email,
+      password: user.password,
+      username: user.username,
+    });
+
+    expect(!!res?.accessToken).toBeTruthy();
+  });
+
+  it('should be called delete method', async () => {
+    const user = await controller.register(newUser);
   });
 });
